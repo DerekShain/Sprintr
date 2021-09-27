68b3e04a66c6df9a9c58930daf67910d48abe4f4
Derek Shain <derek0anthony@gmail.com>
2021-09-27 15:08:22 -0600

    DB and Task

--- Sprintr/server/db/DbContext.js
+++ Sprintr/server/db/DbContext.js
@@ -1,11 +1,17 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
+import { BacklogItemSchema } from '../models/BacklogItem'
+import { NoteSchema } from '../models/Note'
+import { TaskSchema } from '../models/Task'
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
+  BacklogItem = mongoose.model('Backlog', BacklogItemSchema);
+  Note = mongoose.model('Note', NoteSchema);
+  Task = mongoose.model('Task', TaskSchema)
 }
 
 export const dbContext = new DbContext()
