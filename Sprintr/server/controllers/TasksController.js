a13930297a05c85eac57dea29065f34d9c61d6c7
drewmcreynolds <drew.gmcreynolds@gmail.com>
2021-09-27 15:48:17 -0600

    sprint task controller and service

---
+++ Sprintr/server/controllers/TasksController.js
@@ -0,0 +1,64 @@
+import { Auth0Provider } from '@bcwdev/auth0provider'
+import { tasksService } from '../services/TasksService.js'
+import BaseController from '../utils/BaseController.js'
+import { logger } from '../utils/Logger.js'
+
+export class TasksController extends BaseController {
+  constructor() {
+    super('api/tasks')
+    this.router
+      .get('', this.getTasks)
+      .get('/:taskId', this.getTask)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.createTask)
+      .delete('/:taskId', this.removeTask)
+      .put('/:taskId', this.editTask)
+  }
+
+  async getTasks(req, res, next) {
+    try {
+      const tasks = await tasksService.getTasks(req.query)
+      res.send(tasks)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getTask(req, res, next) {
+    try {
+      const task = await tasksService.getTaskById(req.params.taskId)
+      res.send(task)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createTask(req, res, next) {
+    try {
+      logger.log('who is the user?', req.userInfo)
+      req.body.creatorId = req.userInfo.id
+      const task = await tasksService.createTask(req.body)
+      res.send(task)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async removeTask(req, res, next) {
+    try {
+      const task = await tasksService.removeTask(req.params.taskId, req.userInfo.id)
+      res.send(task)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editTask(req, res, next) {
+    try {
+      const task = await tasksService.editTask(req.params.taskId, req.userInfo.id, req.body)
+      res.send(task)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
---
+++ Sprintr/server/services/SprintsService.js
@@ -0,0 +1,47 @@
+import { dbContext } from '../db/DbContext.js'
+import { BadRequest, Forbidden } from '../utils/Errors.js'
+
+class SprintsService {
+  async getSprintById(sprintId) {
+    const sprint = await dbContext.Sprints.findById(sprintId).populate('creator', 'name picture')
+    if (!sprint) {
+      throw new BadRequest('Invalid Sprint Id')
+    }
+    return sprint
+  }
+
+  async editSprint(sprintId, userId, sprintData) {
+    const sprint = await this.getSprintById(sprintId)
+    if (userId !== sprint.creatorId.toString()) {
+      throw new Forbidden('You shall not pass!!!')
+    }
+    sprint.name = sprintData.name || sprint.name
+    sprint.startDate = sprintData.startDate || sprint.startDate
+    sprint.endDate = sprintData.endDate || sprint.endDate
+    sprint.description = sprintData.description || sprint.description
+    sprint.creatorId = sprintData.creatorId || sprint.creatorId
+    await sprint.save()
+    return sprint
+  }
+
+  async removeSprint(sprintId, userId) {
+    const sprint = await this.getSprintById(sprintId)
+    if (userId !== sprint.creatorId.toString()) {
+      throw new Forbidden('You shall not pass!!!')
+    }
+    await sprint.remove()
+    return sprint
+  }
+
+  async createSprint(sprintData) {
+    const sprint = await dbContext.Sprints.create(sprintData)
+    return sprint
+  }
+
+  async getSprints(query) {
+    const sprints = await dbContext.Sprints.find(query).populate('creator', 'name picture')
+    return sprints
+  }
+}
+
+export const sprintsService = new SprintsService()
