2bdb5807411ba7744d549196681b70487512785f
drewmcreynolds <drew.gmcreynolds@gmail.com>
2021-09-27 15:08:24 -0600

    dbContect projects and sprints

--- Sprintr/server/db/DbContext.js
+++ Sprintr/server/db/DbContext.js
@@ -1,11 +1,15 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
+import { ProjectSchema } from '../models/Project.js'
+import { SprintSchema } from '../models/Sprint.js'
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
+  Projects = mongoose.model('Project', ProjectSchema);
+  Sprints = mongoose.model('Sprint', SprintSchema);
 }
 
 export const dbContext = new DbContext()
