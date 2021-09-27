4743a1f9655e4e89b2b5a2bc88b47b8e41ed6a4f
drewmcreynolds <drew.gmcreynolds@gmail.com>
2021-09-27 15:15:30 -0600

    dbConext done

--- Sprintr/server/db/DbContext.js
+++ Sprintr/server/db/DbContext.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
 import { BacklogItemSchema } from '../models/BacklogItem'
 import { NoteSchema } from '../models/Note'
+import { ProjectSchema } from '../models/Project.js'
+import { SprintSchema } from '../models/Sprint.js'
 import { TaskSchema } from '../models/Task'
 import { ValueSchema } from '../models/Value'
 
@@ -9,9 +11,10 @@ class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
-  BacklogItem = mongoose.model('Backlog', BacklogItemSchema);
-  Note = mongoose.model('Note', NoteSchema);
-  Task = mongoose.model('Task', TaskSchema);
+  BacklogItems = mongoose.model('Backlog', BacklogItemSchema);
+  Notes = mongoose.model('Note', NoteSchema);
+  Tasks = mongoose.model('Task', TaskSchema);
+  Projects = mongoose.model('Project', ProjectSchema);
+  Sprints = mongoose.model('Sprint', SprintSchema);
 }
-
 export const dbContext = new DbContext()
