6e78e2baa46567b6fbf81a11655c072f26866145
Derek Shain <derek0anthony@gmail.com>
2021-09-28 13:45:35 -0600

    started backlog

---
+++ Sprintr.client/src/components/Backlog.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      Project Name, Weight
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">
+        0 out of tasks 0 completed
+      </h5>
+      <p class="card-text">
+        Dates
+      </p>
+      <a href="#" class="btn btn-primary">See Details</a><a href="#" class="btn btn-primary">Add Task</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
