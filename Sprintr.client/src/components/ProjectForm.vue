cec74736dda0d6739a28de51d0e588cc2531f5e1
drewmcreynolds <drew.gmcreynolds@gmail.com>
2021-09-28 13:39:33 -0600

    building project stuff

---
+++ Sprintr.client/src/components/ProjectForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <form @submit.prevent="createProject()">
+    <div class="form-group">
+      <label for="body">Name</label>
+      <input type="text"
+             class="form-control bg-light"
+             name="body"
+             placeholder="Body"
+             v-model="editable.name"
+             required
+      >
+    </div>
+    <div class="form-group">
+      <label for="title">Description</label>
+      <input type="test"
+             class="form-control bg-light"
+             name="imgUrl"
+             placeholder="Img Url"
+             v-model="editable.description"
+             required
+      >
+    </div>
+    <div class="form-group">
+      <button type="submit" class="btn btn-success mt-3">
+        Create Project
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+import { Modal } from 'bootstrap'
+import Pop from '../utils/Pop.js'
+import { ref } from '@vue/reactivity'
+import { }
+
+export default {
+  setup() {
+    const editable = ref({})
+    return {
+      editable,
+      async createProject() {
+        try {
+          await projectsService.createProject(editable.value)
+          editable.value = {}
+          Pop.toast('You made it', 'success')
+
+          const modal = Modal.getInstance(document.getElementById('project-form'))
+          modal.hide()
+        } catch (error) {
+          Pop.toast(error, 'error')
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ Sprintr.client/src/services/ProjectsService.js
@@ -0,0 +1,14 @@
+import { AppState } from '../AppState.js'
+import { logger } from '../utils/Logger.js'
+import { api } from './AxiosService.js'
+import { Project } from '../models/Project.js'
+
+class ProjectsService {
+  async createProject(newProject) {
+    const res = await api.post('api/projects', newProject)
+    AppState.projects.unshift(new Project(res.data))
+    logger.log('create project res', res)
+  }
+}
+
+export const projectService = new ProjectsService()
