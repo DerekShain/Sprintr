f016f24e6ae7848f593bf8254abca03eac24a935
Derek Shain <derek0anthony@gmail.com>
2021-09-28 20:57:48 -0600

    this is neat

--- Sprintr.client/src/components/Project.vue
+++ Sprintr.client/src/components/Project.vue
@@ -1,5 +1,60 @@
 <template>
-  <div class="row">
+  <div class="card text-center">
+    <div class="card-header">
+      <div class="collapse" id="navbarToggleExternalContent">
+        <div class="bg-dark p-4">
+          <li class="nav-item">
+            <router-link :to="{name:'Project.Backlog'}" class="text-white lighten-30 bg-dark nav-link active">
+              Backlogs
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{name:'Project.Sprint'}" class="text-white bg-dark lighten-30 nav-link active">
+              Sprints
+            </router-link>
+          </li>
+        </div>
+      </div>
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <button class="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarToggleExternalContent"
+                  aria-controls="navbarToggleExternalContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                  title="Menu"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+      <ul class="nav nav-tabs card-header-tabs">
+        <!-- <li class="nav-item">
+          <router-link :to="{name:'Project.Backlog'}" class="text-dark lighten-30 nav-link active">
+            Backlogs
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{name:'Project.Sprint'}" class="text-dark lighten-30 nav-link active">
+            Sprints
+          </router-link>
+        </li> -->
+      </ul>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">
+        {{ project.name }}
+      </h5>
+      <p class="card-text">
+        {{ project.description }}}}
+      </p>
+      <router-view />
+    </div>
+  </div>
+
+  <!-- <div class="row">
     <div class="card">
       {{ project.name }}
       <p>{{ project.description }}</p>
@@ -11,7 +66,7 @@
       </router-link>
     </div>
     <router-view />
-  </div>
+  </div> -->
 </template>
 
 <script>
